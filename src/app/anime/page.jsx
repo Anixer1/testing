bd23110ffb461be150ba8323eef975f52ff38268
Anixer1 <165531263+Anixer1@users.noreply.github.com>
2024-05-05 16:22:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/anime/page.jsx
@@ -0,0 +1,15 @@
+import styles from "./anime.module.css";
+import Trending from "./top-airing/page";
+import Releases from "./recent/page";
+import Input from "./search/page";
+
+export default async function Anime() {
+	return (
+		<div className={styles.main}>
+			<Input />
+			<Trending />
+			<br />
+			<Releases />
+		</div>
+	);
+}
