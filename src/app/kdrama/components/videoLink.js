b6df1f9995f4ffeb455b12ffa1c1890f216ec7a7
Anixer1 <165531263+Anixer1@users.noreply.github.com>
2024-05-06 01:31:07 +0800

    Update videoLink.js

--- src/app/kdrama/components/videoLink.js
+++ src/app/kdrama/components/videoLink.js
@@ -2,7 +2,7 @@
 export default async function getVideoLink(epiId, mediaId) {
 	let videoLink;
 	const res = await fetch(
-		`https://consumet-api-ro77.onrender.com/movies/dramacool/watch?episodeId=${epiId}&mediaId=${mediaId}`,
+		`https://rayflix-api.vercel.app/movies/dramacool/watch?episodeId=${epiId}&mediaId=${mediaId}`,
 		{ cache: "force-cache" }
 	);
 	const data = await res.json();
